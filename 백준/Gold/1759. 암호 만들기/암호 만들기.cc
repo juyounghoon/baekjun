996816215b0d9892c5c72a8d7f5f1c44f99ea88d
juyounghoon <juyh9808@gmail.com>
2025-03-02 16:36:00 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int L, C;
+vector<char> v;
+
+vector<bool> visited;
+void MakeComb(int index, int cnt, vector<char>& comb);
+bool CheckJaMo(vector<char> comb);
+
+int main(){
+    cin >> L >> C;
+    char input;
+    for(int i = 0; i<C; i++){
+        cin >> input;
+        v.push_back(input);
+    }
+    visited.resize(C, false);
+    sort(v.begin(), v.end());
+    
+    vector<char> comb;
+    MakeComb(0, 0, comb);
+
+    return 0;
+}
+
+void MakeComb(int index, int cnt, vector<char>& comb){
+    if(cnt >= L){
+        if(CheckJaMo(comb)){
+            for(int i = 0; i<L; i++){
+                cout << comb[i];
+            }
+            cout << "\n";
+        }
+        return;
+    }
+
+    for(int i = index; i<C; i++){
+        if(visited[i]){
+            continue;
+        }
+        visited[i] = true;
+        comb.push_back(v[i]);
+        MakeComb(i, cnt+1, comb);
+        comb.pop_back();
+        visited[i] = false;
+    }
+}
+
+bool CheckJaMo(vector<char> comb){
+    bool ret = false;
+    int cnt_ja = 0; // 2개 이상
+    int cnt_mo = 0; // 1개 이상
+    for(int i = 0; i<comb.size(); i++){
+        // a e i o u
+        if(comb[i] == 'a' || comb[i] == 'e'|| comb[i] == 'i' || comb[i] == 'o' || comb[i] == 'u'){
+            cnt_mo++;
+        }
+        else{
+            cnt_ja++;
+        }
+    }
+    if(cnt_ja >= 2 && cnt_mo >= 1){
+        ret = true;
+    }
+
+    return ret;
+}
