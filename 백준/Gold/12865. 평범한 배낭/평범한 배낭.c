d4b5d1076930581d8010fafdaaf61066a2f58c88
juyounghoon <juyh9808@gmail.com>
2025-05-17 22:00:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 64 ms, Memory: 40568 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_N 101
+#define MAX_W 100001
+
+typedef struct Thing{
+    int w;
+    int v;
+}Thing;
+
+Thing things[MAX_N];
+int BackPack[MAX_N][MAX_W] = {-1};
+
+int get_max(int a, int b){
+    if(a>b) return a;
+    else return b;
+}
+
+int BP(int n, int w){
+    if(n<0 || w<0){
+        return 0;
+    }
+    if(BackPack[n][w] != -1){
+        return BackPack[n][w];
+    }
+    int include, except;
+    if(n-1<0 ||  w-things[n].w < 0) include = 0;
+    else include = BP(n-1, w-things[n].w) + things[n].v;
+
+    if(n-1<0) except = 0;
+    else except = BP(n-1, w);
+    BackPack[n][w] = get_max(include, except);;
+    return BackPack[n][w];
+}
+
+int main(){
+    int N, K;
+    scanf("%d %d", &N, &K);
+
+    for(int i = 1; i<=N; i++){
+        int tmp_w, tmp_v;
+        scanf("%d %d", &tmp_w, &tmp_v);
+        things[i].w = tmp_w;
+        things[i].v = tmp_v;
+    }
+    memset(BackPack, -1, sizeof(BackPack));
+    for(int i = 0; i<=K; i++){
+        BackPack[0][i] = 0;
+    }
+    int ans = BP(N, K);
+    printf("%d", ans);
+
+    return 0;
+}
+/*
+BP(n,w) = max(BP(n-1, w-things[i].w) + things[i].v, BP(n-1, w))
+*/
