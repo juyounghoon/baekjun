0c35445f121cc1e982ebe80271221c31e0549fb6
juyounghoon <juyh9808@gmail.com>
2025-05-17 23:16:14 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 1504 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.c
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.c
@@ -1,52 +1,36 @@
 #include <stdio.h>
-#include <string.h>
 
-#define MAX_N 101
-#define MAX_W 100001
+#define MAX_N 100
+#define MAX_W 100000
 
+int N, K;
+int DP[MAX_W+1]; // [w]: w무게일 때 최대 value
 typedef struct Thing{
-    int w;
-    int v;
+    int w, v;
 }Thing;
 
-Thing things[MAX_N];
-int BackPack[MAX_N][MAX_W] = {-1};
-
-int BP(int n, int w){
-    if(n<0 || w<0) return 0;
-    if(BackPack[n][w] == -1){
-        int include, except;
-        if(n-1<0 ||  w-things[n].w < 0) include = 0;
-        else include = BP(n-1, w-things[n].w) + things[n].v;
-
-        if(n-1<0) except = 0;
-        else except = BP(n-1, w);
-
-        BackPack[n][w] = include < except ? except : include;
-    }
-    return BackPack[n][w];
+int get_max(int a, int b){
+    if(a>b) return a;
+    else return b;
 }
 
 int main(){
-    int N, K;
     scanf("%d %d", &N, &K);
-
-    for(int i = 1; i<=N; i++){
-        int tmp_w, tmp_v;
-        scanf("%d %d", &tmp_w, &tmp_v);
-        things[i].w = tmp_w;
-        things[i].v = tmp_v;
+    Thing things[MAX_N];
+    for(int i = 0; i<N; i++){
+        scanf("%d %d", &things[i].w, &things[i].v);
     }
-
-    memset(BackPack, -1, sizeof(BackPack));
-    for(int i = 0; i<=K; i++){
-        BackPack[0][i] = 0;
+    for(int i = 0; i<N; i++){
+        int w = things[i].w;
+        int v = things[i].v;
+        for(int j = K; j>=w; j--){
+            DP[j] = get_max(DP[j], DP[j-w] + v);
+        }
     }
-
-    printf("%d", BP(N, K));
-
+    printf("%d", DP[K]);
+    
     return 0;
 }
 /*
-BP(n,w) = max(BP(n-1, w-things[i].w) + things[i].v, BP(n-1, w))
+dp[w+things[i].w] = dp[w+things[i].w] or dp[w] + things[i].v
 */
